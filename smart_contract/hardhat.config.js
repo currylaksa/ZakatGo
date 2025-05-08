67e0bca399b150e9b67452477d096cdecc539e3d
currylaksa <qingyee0219@gmail.com>
2025-05-08 15:51:40 +0800

    updated

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -10,5 +10,12 @@ module.exports = {
         "ba988cc7807b6be4ab7170d45a2423cd044814ea783f85d1e0b04fa6a9563245",
       ],
     },
+    swanSaturn: {
+      url: "http://185.199.53.44:8545",
+      accounts: [
+        "d2e041aa677d63a1ccf82d8875989d5cf01e871cf9d27864d48b18fe02d72d27",
+      ],
+      chainId: 2024
+    },
   },
 };
