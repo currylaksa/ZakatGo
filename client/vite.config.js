ed0d89569ab064a58094cd95a28901900ffa4bf6
currylaksa <qingyee0219@gmail.com>
2025-05-10 16:20:44 +0800

    Revert "deploy in vercel"
    This reverts commit e1469e22290c8aee46bae932664367f6bd5360fb.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: 'terser',
-    sourcemap: false,
-    // Simplify the build process
-    chunkSizeWarningLimit: 1600,
-  },
-  // Fix for potential caching issues
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true
-  }
-})
+});
